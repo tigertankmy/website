26f1c041a29d4ab8540791a56a1814f5fe570ed1
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2026-03-06 01:30:02 +0800

    mobile version (should be) done - changed nav bar text links to image icons if on small screens - separated size variable into base font size & base width

--- load-navigation-bar.js
+++ load-navigation-bar.js
@@ -1,19 +1,48 @@
-const links = {
-	"/": "Home",
-	"/register": "Register",
-	"/resources": "Resources",
-	"/faq": "FAQ",
-	"sponsors-and-partners": "Sponsors & partners",
-};
+const links = [
+	[
+		"/",
+		"Home",
+		"./assets/icons/home_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
+	],
+	[
+		"/register",
+		"Register",
+		"./assets/icons/checkbook_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
+	],
+	[
+		"/resources",
+		"Resources",
+		"./assets/icons/books_movies_and_music_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
+	],
+	[
+		"/faq",
+		"FAQ",
+		"./assets/icons/contact_support_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
+	],
+	[
+		"sponsors-and-partners",
+		"Sponsors & partners",
+		"./assets/icons/handshake_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
+	],
+];
 
 const ul = document.createElement("ul");
 
-for (const [href, name] of Object.entries(links)) {
+for (const [href, name, iconPath] of links) {
 	const li = document.createElement("li");
 	const a = document.createElement("a");
 	a.href = href;
-	a.textContent = name;
 
+	const img = document.createElement("img");
+	img.classList.add("nav-link-icon");
+	img.src = iconPath;
+
+	const span = document.createElement("span");
+	span.classList.add("nav-link-anchor-text");
+	span.textContent = name;
+
+	a.appendChild(img);
+	a.appendChild(span);
 	li.appendChild(a);
 	ul.appendChild(li);
 }
