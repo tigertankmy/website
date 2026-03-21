127ebbf412797b9dea7541d75939e17e7ac682bc
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2026-03-21 19:57:37 +0800

    add download function wow

--- admin/social-media-post-generator.js
+++ admin/social-media-post-generator.js
@@ -1,14 +1,12 @@
 /* eslint-disable no-loop-func */
 import updateCanvas from "./scripts/updateCanvas.js";
 // eslint-disable-next-line max-len
-const DEFAULT_TEXTAREA_TEXT = `#Heading text begins with a hashtag (#)
+const DEFAULT_TEXTAREA_TEXT = `#This is a heading
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum vehicula tristique. Vivamus est eros, mattis vitae arcu ac, accumsan commodo risus. Ut vitae luctus magna, eu placerat nisl. Vivamus non ante sodales, semper odio vitae, ultrices nisi.
 Mauris posuere leo justo, non fringilla quam semper eu. In ipsum enim, malesuada a diam non, feugiat lacinia erat. Integer vel mi et nulla fringilla gravida non ac neque.
 Nam iaculis, dolor ut efficitur semper, lectus diam rhoncus nulla, id consequat justo velit in lorem.
 Nulla auctor eros ac diam accumsan pellentesque. Fusce ornare mi ex, vel egestas neque vulputate id. Quisque ut molestie felis, vel bibendum justo. Sed posuere magna auctor urna luctus, a condimentum leo consequat. Vestibulum vehicula tellus dapibus purus condimentum consectetur.
-
-
-#Another heading!
+#This is another heading bleep bleep blorp blorp
 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec at urna porta, efficitur augue eget, tristique elit. Aliquam interdum efficitur metus. Aenean sit amet porta libero. Phasellus iaculis nulla sem. Aliquam laoreet vel ligula at porttitor. Donec dapibus lacinia erat nec hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus.`;
 const DEFAULT_FONT_FAMILY = "Playfair Display";
 const DEFAULT_FONT_SIZE = 42;
@@ -50,13 +48,17 @@ for (const [elementName, htmlId] of Object.entries(DOMelementsThatUpdateCanvasOn
 	if (htmlId === "image-width-input") {
 		element.value = DEFAULT_IMAGE_WIDTH;
 		callback = (ev) => {
-			canvas.width = ev.target.value;
+			for (const canvasElement of document.querySelectorAll("canvas")) {
+				canvasElement.width = ev.target.value;
+			}
 			updateCanvas(ctx, fontFamilyName, DOMelementsThatUpdateCanvasOnInput);
 		};
 	} else if (htmlId === "image-height-input") {
 		element.value = DEFAULT_IMAGE_HEIGHT;
 		callback = (ev) => {
-			canvas.height = ev.target.value;
+			for (const canvasElement of document.querySelectorAll("canvas")) {
+				canvasElement.height = ev.target.value;
+			}
 			updateCanvas(ctx, fontFamilyName, DOMelementsThatUpdateCanvasOnInput);
 		};
 	} else if (htmlId === "font-family-input") {
@@ -92,4 +94,16 @@ window.addEventListener("load", () => {
 	DOMelementsThatUpdateCanvasOnInput.fontFamilyInput.dispatchEvent(new Event("input"));
 	DOMelementsThatUpdateCanvasOnInput.imageWidthInput.dispatchEvent(new Event("input"));
 	DOMelementsThatUpdateCanvasOnInput.imageHeightInput.dispatchEvent(new Event("input"));
+});
+
+document.getElementById("download-button").addEventListener("click", () => {
+	for (const [index, canvasElement] of Array.from(document.querySelectorAll("canvas")).entries()) {
+		canvasElement.toBlob((blob) => {
+			const a = document.createElement("a");
+			a.href = window.URL.createObjectURL(blob);
+			// remember, indexes start from 0
+			a.download = `Tiger Tank social media post - image ${index + 1}`;
+			a.click();
+		});
+	}
 });
